2103116fbd0d1f96d4628f5daf493ab857de49bd
arahmaninov <arahmaninov@yandex.ru>
2022-08-11 11:23:52 +0300

    add: class Point

---
+++ TetrisCS/TetrisCS/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace TetrisCS
+{
+    class Point
+    {
+        int x;
+        int y;
+        char c;
+
+        public Point(int a, int b, char symbol)
+        {
+            x = a;
+            y = b;
+            c = symbol;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
--- TetrisCS/TetrisCS/Program.cs
+++ TetrisCS/TetrisCS/Program.cs
@@ -6,7 +6,16 @@ namespace TetrisCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(35, 40);
+            Console.SetBufferSize(35, 40);
+
+            Point p1 = new Point(2, 4, '@');
+            p1.Draw();
+
+            Point p2 = new Point(8, 14, '^');
+            p2.Draw();
+
+            Console.ReadLine();
         }
     }
 }
